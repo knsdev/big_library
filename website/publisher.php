189b194d28aa59e0ab205e6109f6c403563ffde1
Kim <kimniklas.schlueter@gmail.com>
2025-07-19 12:02:28 +0200

    Create folders for different file types

--- website/publisher.php
+++ website/publisher.php
@@ -34,7 +34,7 @@ $page_title = "Media published by $publisher_name";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>$page_title</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-  <link rel="stylesheet" href="media_card_layout.css">
+  <link rel="stylesheet" href="./styles/media_card_layout.css">
 </head>
 
 <body>
